e3cf14d2db5a893eb8d5a1dbde067879f195d9a1
phobyjun@gmail.com <phobyjun@gmail.com>
2021-12-07 00:26:52 +0900

    ADD: Test files ADD: Run step on github-action

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+	targetDir := "./TIL"
+	fileHistory := ".history"
+	posts := getNewPosts(targetDir, fileHistory)
+	for _, post := range posts {
+		fmt.Println(post)
+	}
+
 	user := "phobyjun"
 	token := os.Getenv("MY_GITHUB_TOKEN")
 
@@ -21,21 +30,6 @@ func main() {
 	err := autoCreatePost(ctx, client, user, fileContent, filePath)
 	checkErr(err)
 
-	targetDir := "./TIL"
-	files := getCurrentFiles(targetDir)
-	for _, file := range files {
-		fmt.Println(file)
-	}
-}
-
-func getCurrentFiles(targetDir string) []string {
-	files, err := ioutil.ReadDir(targetDir)
-	checkErr(err)
-	var f []string
-	for _, file := range files {
-		f = append(f, file.Name())
-	}
-	return f
 }
 
 func getClientByToken(ctx context.Context, token string) *github.Client {
@@ -66,6 +60,52 @@ func readMarkDownToByte(filePath string) []byte {
 	content, err := ioutil.ReadFile(filePath)
 	checkErr(err)
 	return content
+}
+
+func getNewPosts(targetDir, fileHistory string) []string {
+	files := getCurrentFiles(targetDir)
+	history := getFileHistory(fileHistory)
+	posts := difference(files, history)
+	return posts
+}
+
+func getFileHistory(fileHistory string) []string {
+	var history []string
+	file, err := os.Open(fileHistory)
+	checkErr(err)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		history = append(history, scanner.Text())
+	}
+	return history
+}
+
+func getCurrentFiles(targetDir string) []string {
+	var files []string
+	err := filepath.Walk(targetDir,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			files = append(files, path)
+			return nil
+		})
+	checkErr(err)
+	return files
+}
+
+func difference(a, b []string) []string {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[x] = struct{}{}
+	}
+	var diff []string
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
 }
 
 func checkErr(e error) {
